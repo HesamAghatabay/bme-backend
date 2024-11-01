f94793bad505e0e80a479e7420487a3aec666be3
Hesam Aghatabay <program.h.p.2023@gmail.com>
2024-11-01 23:14:19 +0330

    updating

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,92 @@
+<footer>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-6 col-md-3">
+                <h3 class="text-start mb-4 my-5">دسته بندی</h3>
+                <ul class="ul-none">
+                    <li class="my-3"><a href="" class="a-bold irsan">دسته 1</a></li>
+                    <li class="my-3"><a href="" class="a-bold irsan">دسته 2</a></li>
+                    <li class="my-3"><a href="" class="a-bold irsan">دسته 3</a></li>
+                    <li class="my-3"><a href="" class="a-bold irsan">دسته 4</a></li>
+                    <li class="my-3"><a href="" class="a-bold irsan">دسته 5</a></li>
+                    <li class="my-3"><a href="" class="a-bold irsan">دسته 6</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-3 my-5">
+                <h3 class="text-start mb-4">فهرست مقالات</h3>
+                <ul class="ul-none">
+                    <li class="my-3"><a href="" class="a-bold irsan"> مقالات پرمخاطب</a></li>
+                    <li class="my-3"><a href="" class="a-bold irsan"> مقالات جدید</a></li>
+                    <li class="my-3"><a href="" class="a-bold irsan"> مقالات</a></li>
+                    <li class="my-3"><a href="" class="a-bold irsan"></a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-5 my-5 text-end">
+                <h3 class="text-start mb-4">راه های انحرافی</h3>
+
+                <div class="row row-cols-2 justify-content-between align-items-center my-2">
+                    <div class="col d-flex align-items-center">
+                        <i class="bi bi-browser-chrome f-20"></i>
+                        <p class="d-flex mx-1">وبسایت: </p>
+                    </div>
+                    <div class="col">
+                        <a href="https://shahroodut.ac.ir/fa/">www.shahroodut.ac.ir</a>
+                    </div>
+                </div>
+
+                <div class="row row-cols-2 justify-content-between align-items-center my-2">
+                    <div class="col d-flex align-items-center">
+                        <i class="bi bi-telephone-fill f-20"></i>
+                        <p class="d-flex mx-1">تماس: </p>
+                    </div>
+                    <div class="col">
+                        <a href="tel:+982332392204">9 - 02332392204</a>
+                    </div>
+                </div>
+
+                <div class="row row-cols-2 justify-content-between align-items-center my-2">
+                    <div class="col d-flex align-items-center">
+                        <i class="bi bi-mailbox2-flag f-20"></i>
+                        <p class="d-flex mx-1">کدپستی: </p>
+                    </div>
+                    <div class="col">
+                        <p>3619995161</p>
+                    </div>
+                </div>
+
+                <div class="row row-cols-2 justify-content-between align-items-center my-2">
+                    <div class="col d-flex align-items-center">
+                        <i class="bi bi-envelope-at-fill f-20"></i>
+                        <p class="d-flex mx-1">ایمیل: </p>
+                    </div>
+                    <div class="col">
+                        <p>info@shahroodut.ac.ir</p>
+                    </div>
+                </div>
+
+                <div class="cantact-footer-div my-4 d-flex justify-content-end align-items-center">
+                    <a href="" class="mx-2"><i class="i-footer bi bi-instagram"></i></a>
+                    <a href="" class="mx-2"><i class="i-footer bi bi-twitter"></i></a>
+                    <a href="" class="mx-2"><i class="i-footer bi bi-telegram"></i></a>
+                    <a href="" class="mx-2"><i class="i-footer bi bi-facebook"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+</main>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
+<script src="./js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>index</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css"
+        integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+
+</head>
+
+<body>
+
+    
+    <!-- mobile header -->
+
+    <header class="d-flex d-md-none shadow">
+        <nav class="shadow">
+            <div class="container-fluid">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-5">
+                        <div class="row row-cols-3">
+                            <div class="col">
+                                <a class="button-87s" href="register">ثبت نام</a>
+                            </div>
+                            <div class="col">
+                                <a class="button-87s" href="login">ورود</a>
+                            </div>
+                            <div class="col">
+                                <a class="button-87s" href="add-article">افزودن </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="serch-navs shadow">
+                            <form action="" class="d-flex justify-content-center">
+                                <input type="search" class="form-control" placeholder="جست و جو">
+                                <button type="submit" class="btn btn-primary"><i class="bi bi-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="div-header-s">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-11 text-center name-header animate__animated animate__fadeInUp">
+                        <h1>مهندسی پزشکی شاهرود</h1>
+                        <h6>وبسایت رسمی مقالات مهندسی پزشکی و کمک توانبخشی دانشگاه صنعتی شاهرود</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <!-- desktop hader -->
+    <header class="d-none d-md-flex shadow">
+        <nav class="shadow">
+            <div class="container-fluid text-center">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col col-3">
+                        <a class="button-87" href="register">ثبت نام</a>
+                        <a class="button-87" href="login">ورود</a>
+                        <a class="button-87" href="add-article">افزودن مقاله</a>
+                    </div>
+                    <div class="col col-6">
+                        <ul class="d-md-flex justify-content-center align-items-center">
+                            <li class="mx-4"><a class="vazir" href="#home">خانه</a></li>
+                            <!-- <li class="mx-4"><a class="a-bold" href="#parts">دسته ها</a></li> -->
+                            <li class="mx-4">
+                                <div class="dropdown vazir">
+                                    <h6 class="dropdown-toggle h6-dropdown" data-bs-toggle="dropdown"
+                                        aria-expanded="false">
+                                        دسته ها
+                                    </h6>
+                                    <ul class="dropdown-menu dropdown-bg">
+                                        <li><a class="dropdown-item mt-1" href="#">دسته اول</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">دسته دوم</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">دسته سوم</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">دسته چهارم</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">دسته پنجم</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">دسته ششم</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <!-- <li class="mx-4"><a class="a-bold" href="#articles">مقالات</a></li> -->
+                            <li class="mx-4">
+                                <div class="dropdown vazir">
+                                    <h6 class="dropdown-toggle h6-dropdown" data-bs-toggle="dropdown"
+                                        aria-expanded="false">
+                                        مقالات
+                                    </h6>
+                                    <ul class="dropdown-menu dropdown-bg">
+                                        <li><a class="dropdown-item mt-1" href="#">مقالات پرطرفدار</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">مقالات جدید</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">مقالات دسته اول</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">مقالات دسته دوم</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">مقالات دسته سوم</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">مقالات دسته چهارم</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">مقالات دسته پنجم</a></li>
+                                        <li><a class="dropdown-item mt-1" href="#">مقالات دسته ششم</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="mx-4"><a class="vazir" href="#about">درباره ما</a></li>
+                        </ul>
+                    </div>
+                    <div class="col col-3">
+                        <!-- <img class="logo-img shadow" src="./imgs/logo.jpg" alt="bme-logo"> -->
+                        <div class="serch-nav shadow">
+                            <form action="" class="d-flex justify-content-center">
+                                <input type="search" class="form-control" placeholder="مقالات خود را جست و جو کنید">
+                                <button type="submit" class="btn btn-primary"><i class="bi bi-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="div-header text-center">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-6 name-header animate__animated animate__fadeInUp">
+                        <h1>مهندسی پزشکی شاهرود</h1>
+                        <h6>وبسایت رسمی مقالات مهندسی پزشکی و کمک توانبخشی دانشگاه صنعتی شاهرود</h6>
+                    </div>
+                </div>
+
+
+
+                <div class="row justify-content-center">
+                    <div class="col-5">
+                        <div class="serch-header shadow animate__animated animate__fadeInUpBig">
+                            <form action="" class="d-flex justify-content-center">
+                                <input type="search" class="form-control" placeholder="مقالات خود را جست و جو کنید">
+                                <button type="submit" class="btn btn-primary"><i class="bi bi-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
