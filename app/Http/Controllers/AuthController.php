f4666154c1806269023592337ec47d041e7fdebc
Hesam Aghatabay <program.h.p.2023@gmail.com>
2024-11-18 22:40:54 +0330

    updating

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,7 +14,7 @@ class AuthController extends Controller
     }
     public function store(Request $request)
     {
-        $request->validate([
+        $$credentials = $request->validate([
             'name' => 'required|max:255',
             'phone' => 'required|max:255|unique:users',
             'email' => 'max:255|email',
@@ -26,6 +28,15 @@ class AuthController extends Controller
             'email.max' => '*بیش از حد مجاز*',
             'email.email' => '*ایمیل خود را به صورت صحیح وارد کنید*',
             'password.required' => '*ایجاد رمز الزامی است*',
+            'password.min' => '*حداقل 8 کارکتر*',
+            'password_confirmation.required' => '*تاییده رمز الزامی است*',
+            'password_confirmation.password_confirmation' => 'تاییده رمز را به صورت صحیح وارد کنید'
         ]);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->back()->with('error', 'ثبت نام با خطا مواجه شد');
+        }
+
+        return redirect()->route('login')->with('sucsess', 'ثبت نام با موفقیت انجام شد . لطفا برای دسترسی به خدمات وارد سایت شوید');
     }
 }
