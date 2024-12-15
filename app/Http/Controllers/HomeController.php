571e847f053807be53ea65cd2a5aa89910f6566c
Hesam Aghatabay <program.h.p.2023@gmail.com>
2024-12-15 09:53:10 +0330

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,13 +13,18 @@ class HomeController extends Controller
     public function index()
     {
         $articles = article::where('activity', 1)->get();
-        $newarticles = article::latest()->take(8)->get();
-        $bestarticles = DB::table('articles')->orderBy('likes', 'desc')->take(6)->get();
+        $newarticles = article::latest()->where('activity', 1)->take(10)->get();
+        $bestarticles = DB::table('articles')->where('activity', 1)->orderBy('likes', 'desc')->take(6)->get();
         // $categories = category::find(2);
         $categories = category::all();
-        $categoryHasArticle = Category::take(8)->has('articles')->with('articles')->get();
+        // $categoryHasArticle = Category::limit(10)->has('articles')->get();
         // dd($categories->articles);
-
+        $categoryHasArticle = Category::with(['articles' => function ($query) {
+            $query->where('activity', 1);
+        }])->get();
+        $categoryHasArticle = $categoryHasArticle->filter(function ($category) {
+            return $category->articles->isNotEmpty();
+        });
         // foreach ($categories as $categories) {
         //     $articles = DB::table('articles')->where('category_id', $categories->id)->get();
         // }
