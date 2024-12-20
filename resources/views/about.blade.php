78dd21d09a9880e68f085cd6fe45e60d23275dd6
Hesam Aghatabay <program.h.p.2023@gmail.com>
2024-12-20 22:47:03 +0330

    updating

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,8 +2,32 @@
 @section('content')
     <section id="about" class="text-center mt-130">
         <div class="comtainer px-3">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-12 col-md-6">
+
+
+            <div class="row justify-content-center align-items-center mb-250">
+                <div class="col-10 col-md-4">
+                    <h5>رئیس دانشکده: محمدرضا جوان
+                    </h5>
+                    <p class="text-start"> دانشکده مهندسی برق از بزرگترین دانشکده های دانشگاه شاهرود محسوب می گردد. هسته
+                        اولیه این دانشکده در
+                        سال ۱۳۶۶ بعنوان گروه برق شکل گرفت. در حال حاضر این دانشکده درگرایش های الکترونیک، مخابرات، کنترل،
+                        قدرت و مهندسی پزشکی در مقاطع مختلف کارشناسی و تحصیلات تکمیلی (ارشد، دکتری) دانشجو می پذیرد. در حال
+                        حاضر ۳۲ عضو هیات علمی مجرب شامل ۳ نفر استاد تمام، ۱۲ نفر دانشیار و ۱۷ نفر استادیار بصورت تمام وقت در
+                        این دانشکده فعالیت می نمایند. این دانشکده مجهز به ۳۲ آزمایشگاه آموزشی و تحقیقاتی می باشد. وجود
+                        امکانات آزمایشگاهی و اعضاء هیات علمی مجرب این دانشکده را به فضایی مناسب جهت تربیت دانشجویان در مقاطع
+                        تحصیلی مختلف تبدیل نموده است. کانال رسمی دانشکده در فضای مجازی: https://splus.ir/ee_sut تلفنهای
+                        داخلی آموزش کارشناسی: ۳۲۲۶ داخلی آموزش ارشد: ۳۲۲۳ </p>
+                </div>
+                <div class="col-10 col-md-5">
+                    <img class="rounded shadow w-100 my-4" src="{{ asset('imgs/bargh.jpg') }}" alt="دانشکده برق شاهرود">
+                </div>
+            </div>
+
+
+
+
+            <div class="row justify-content-center align-items-start my-5">
+                <div class="col-10 col-md-5">
                     <video class="video rounded mb-4" muted="" type="video/mp4" id="videoPlayer" controls="">
                         <source src="https://shahroodut.ac.ir/en/sec/files/fl/44/about/about.mp4">
                         Your browser does not support the video tag.
@@ -31,7 +55,7 @@
                             دانشگاه کارآفرین گام بر می‌دارد.</p>
                     </div>
                 </div>
-                <div class="col-12 col-md-6 py-5">
+                <div class="col-10 col-md-5 py-5">
                     <div id="carouselExample" class="carousel slide">
                         <div class="carousel-inner">
 
